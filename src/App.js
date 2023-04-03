c0d59b62d9dbea70dd0f57723a83c0a8e19cb23c
Luoxi Pan <l.panpax@gmail.com>
2023-04-03 16:05:16 +1000

    Add nav links routed to different components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Explore from './components/Explore';
+import Saved from './components/Saved';
 import Footer from './components/Footer';
 import './styles/App.css';
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <Home />
+            <HashRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} exact />
+                    <Route path="/explore" element={<Explore />} exact />
+                    <Route path="/saved" element={<Saved />} exact />
+                </Routes>
+            </HashRouter>
             <Footer />
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
 
 function Header() {
     return (
         <div className="Header">
-            <h1>Header</h1>
+            <span className="Header-title">WalkingTrails</span>
+            <nav>
+                <ul className="nav-list__left">
+                    <Link to="/">
+                        <li>WalkingTrails</li>
+                    </Link>
+                    <Link to="/explore">
+                        <li>Explore</li>
+                    </Link>
+                    <Link to="/saved">
+                        <li>Saved</li>
+                    </Link>
+                </ul>
+            </nav>
+            <nav className="nav-list__right">
+                <ul>
+                    <li>Help</li>
+                    <li>Profile</li>
+                </ul>
+            </nav>
         </div>
     );
 }
