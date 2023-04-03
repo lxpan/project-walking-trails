1481624bd6c7b1fc0fa56bc8763b6ef5bc862575
Luoxi Pan <l.panpax@gmail.com>
2023-04-03 16:04:15 +1000

    Create components Explore and Saved

---
+++ src/components/Explore.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Explore() {
+    return (
+        <div className="Explore">
+            <h1>Explore</h1>
+        </div>
+    );
+}
+
+export default Explore;
---
+++ src/components/Saved.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Favourites() {
+    return (
+        <div className="Saved">
+            <h1>Saved</h1>
+        </div>
+    );
+}
+
+export default Favourites;
