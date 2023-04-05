8bfca0ba0e613f491afe342d5f6972d4b24e539e
Luoxi Pan <l.panpax@gmail.com>
2023-04-05 15:57:17 +1000

    Install firebase and add API keys

--- src/overmind/effects.js
+++ src/overmind/effects.js
@@ -1,3 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBOirK1XQJqP_p_DhEB5kjldjtmu7K9v1c',
+    authDomain: 'walking-trails-e8037.firebaseapp.com',
+    projectId: 'walking-trails-e8037',
+    storageBucket: 'walking-trails-e8037.appspot.com',
+    messagingSenderId: '225102397227',
+    appId: '1:225102397227:web:4ed6326f39f572348e4f6e',
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
 const trailsApi = {
     getTrailsFromServer() {},
 };
