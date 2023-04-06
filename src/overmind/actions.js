a40188b44e3e9bccc5b9ec587f83a8c6c0c9f254
Luoxi Pan <l.panpax@gmail.com>
2023-04-06 11:29:15 +1000

    Add 5s sleep to migration block

--- src/overmind/actions.js
+++ src/overmind/actions.js
@@ -1,8 +1,13 @@
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export const onInitializeOvermind = async ({ state, effects }, migration = false) => {
     effects.api.initialize();
 
     if (migration) {
         await effects.api.wipeTrails();
+        sleep(5000);
         effects.api.migrateTrails();
     }
 
