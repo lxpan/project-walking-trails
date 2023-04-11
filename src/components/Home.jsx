f048aeae532156a428eaba42d7699e3a53d52eba
Luoxi Pan <l.panpax@gmail.com>
2023-04-11 12:20:39 +1000

    Add Overmind context to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAppState, useActions } from '../overmind';
 import '../styles/Home.css';
 import splashImg from '../assets/images/home-splash.jpg';
 // import splashImg from '../assets/images/creswick-forest.jpg';
 
 function Home() {
+    const { trails, loading } = useAppState();
+    const actions = useActions();
+
     const splashStyle = {
         backgroundImage: `url(${splashImg})`,
         backgroundSize: 'cover',
     };
 
+    useEffect(() => {
+        actions.getTrails();
+    }, []);
+
+    useEffect(() => {
+        // console.log(trails);
+    }, [trails]);
+
+    // const trailItems = trails.map((t, index) => <li key={index}>{t.name}</li>);
+
+    if (loading) {
+        return <div className="Home">Loading...</div>;
+    }
+
     return (
         <div className="Home">
             <div className="Home-splash" style={splashStyle}>
                 <h1 className="Home-splash__h1">Are You Ready, Lou?</h1>
-            </div>
-            <div className="Home-trails">
-                <h1>Local favourites near Ballarat</h1>
             </div>
         </div>
     );
