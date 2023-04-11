cb90b6942f265751cebc6da1bc021e3238004fad
Luoxi Pan <l.panpax@gmail.com>
2023-04-11 12:16:04 +1000

    Refactor getTrails to explicitly set state from backend

--- src/overmind/actions.js
+++ src/overmind/actions.js
@@ -1,15 +1,34 @@
+/*
+An action can :
+
+    Mutate state
+    Trigger another action
+    Call a side effect
+    Be async
+*/
+
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export const onInitializeOvermind = async ({ state, effects }, migration = false) => {
+export const onInitializeOvermind = async ({ state, effects }) => {
+    // effects.api.initialize();
+    // state.trails = await effects.api.getTrails();
+};
+
+// calls the backend and loads resulting data into state
+export const getTrails = async ({ state, effects }) => {
+    state.loading = true;
+    effects.api.initialize();
+    const trails = await effects.api.nodeQuery();
+    state.trails = trails;
+    state.loading = false;
+};
+
+export const migrateTrailsData = async ({ effects }) => {
     effects.api.initialize();
 
-    if (migration) {
-        await effects.api.wipeTrails();
-        sleep(5000);
-        effects.api.migrateTrails();
-    }
-
-    state.trails = await effects.api.getTrails();
+    await effects.api.wipeTrails();
+    sleep(5000);
+    effects.api.migrateTrails();
 };
