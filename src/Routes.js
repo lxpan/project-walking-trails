7cc0864c34df8a34e7e78b59a0d5b1c129f8af5f
Luoxi Pan <l.panpax@gmail.com>
2023-04-17 15:10:20 +1000

    Remove craco package

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,28 @@
 import length from '@turf/length';
-import geoJson from './geoJson/buninyong.json';
+// import geoJson from './geoJson/buninyong.json';
+
+// const geoJSON = {
+//     buninyong: {
+//         slug: 'mt-buninyong-walk',
+//         path: './geoJson/buninyong.json',
+//     },
+// };
+
+const geoJson = require('./geoJson/buninyong.json');
+
+console.log(geoJson);
 
 export default function geoRoutes() {
-    // calculate distance using geoJson
+    // Calculate distance using geoJson
     const distance = length(geoJson);
 
     // Calculate elevation gain using coordinate data
-    const { coordinates } = geoJson.features[0].geometry;
     let elevationGain = 0;
+    let posElevationCount = 0;
+    let negElevationCount = 0;
 
-    console.log(`Arr length: ${coordinates[0].length}`);
-
+    const { coordinates } = geoJson.features[0].geometry;
+    // console.log(`Arr length: ${coordinates[0].length}`);
     coordinates[0].forEach((coord, index) => {
         // stop 1 point early since comparison requires 2 points
         if (index === coordinates[0].length - 1) {
@@ -19,10 +31,15 @@ export default function geoRoutes() {
         // console.log(index);
         const elevationDifference = coordinates[0][index + 1][2] - coordinates[0][index][2];
 
+        if (elevationDifference > 0) posElevationCount += 1;
+        if (elevationDifference < 0) negElevationCount += 1;
+
         if (elevationDifference > 0) {
             elevationGain += elevationDifference;
         }
     });
+
+    console.log(`Pos gain count: ${posElevationCount}, neg gain count: ${negElevationCount}`);
 
     return {
         geoJson,
