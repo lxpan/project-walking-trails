05bb046d82f85af8f739dd804ead5e1c969e6b31
Luoxi Pan <l.panpax@gmail.com>
2023-04-18 16:39:01 +1000

    Add realtime database url to config

--- src/overmind/firebaseConfig.js
+++ src/overmind/firebaseConfig.js
@@ -6,6 +6,7 @@ const firebaseConfig = {
     storageBucket: 'walking-trails-e8037.appspot.com',
     messagingSenderId: '225102397227',
     appId: '1:225102397227:web:4ed6326f39f572348e4f6e',
+    databaseURL: 'https://walking-trails-e8037-default-rtdb.asia-southeast1.firebasedatabase.app/',
 };
 
 export default firebaseConfig;
