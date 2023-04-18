bbac99e6987857435cce2512aa3de7c86414a570
Luoxi Pan <l.panpax@gmail.com>
2023-04-18 16:38:47 +1000

    Create geoJSON export with additional routes

---
+++ src/utils/routeImports.js
@@ -0,0 +1,16 @@
+import brandingYard from '../geoJson/branding-yard-you-yangs.json';
+import buninyong from '../geoJson/buninyong.json';
+import cosgroveReservoir from '../geoJson/cosgrove-reservoir-loop.json';
+import creswickCircuitWalk from '../geoJson/creswick-circuit-walk.json';
+import lakeWendoureeNorth from '../geoJson/lake-wendouree-north-park.json';
+import yarroweeRiver from '../geoJson/yarrowee-river-south-trail.json';
+
+const routeImports = {
+    'branding-yard-you-yangs': brandingYard,
+    'mt-buninyong': buninyong,
+    'cosgrove-reservoir-loop': cosgroveReservoir,
+    'creswick-circuit-walk': creswickCircuitWalk,
+    'lake-wendouree-north-park': lakeWendoureeNorth,
+    'yarrowee-river-south': yarroweeRiver,
+};
+export default routeImports;
