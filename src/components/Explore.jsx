a57cf154c8a30224207d260398cb3704cedfdd39
Luoxi Pan <l.panpax@gmail.com>
2023-04-20 11:02:23 +1000

    Create Map component to display map within Explore

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Map from './Map';
 
 function Explore() {
     return (
         <div className="Explore">
             <h1>Explore</h1>
+            <Map />
         </div>
     );
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,47 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '../styles/Map.css';
+
+mapboxgl.accessToken =
+    'pk.eyJ1IjoibHBhbmRldiIsImEiOiJjbGdlZnFvNDEwdTF0M3JyeW5nNjF0bHg2In0.FeOaetmAXx5D4hb1A4e-hg';
+
+function Map() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    // initialise the map right after component load
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/outdoors-v11',
+            center: [lng, lat],
+            zoom,
+        });
+    });
+
+    // store new coordinates as user moves the map
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
+        });
+    });
+
+    return (
+        <>
+            <div className="sidebar">
+                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+            </div>
+            <div ref={mapContainer} className="map-container" />
+        </>
+    );
+}
+
+export default Map;
