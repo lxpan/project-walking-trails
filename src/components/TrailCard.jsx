4bee7f33ab2f78b325e4bb2ea2e0aa2ca62500bb
Luoxi Pan <l.panpax@gmail.com>
2023-05-02 17:49:12 +1000

    Refactor TrailCard component
    - Move JS style code fully into CSS
    - Make clickable
    - Wrap content into a parent div
    - Wrap img into <figure> tag

--- src/components/TrailCard.jsx
+++ src/components/TrailCard.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import placeholderThumbnail from '../assets/images/alltrails-sized-placeholder.jpg';
+import '../styles/TrailCard.css';
 
-function TrailCard() {
+function TrailCard({ trail }) {
     return (
-        <div className="TrailCard">
-            <h1>TrailCard</h1>
-        </div>
+        <a href="#" className="trail-card">
+            <figure>
+                <img src={placeholderThumbnail} alt="" />
+            </figure>
+            <div className="card-body">
+                <h2>{trail.name}</h2>
+                <div>{trail.area}</div>
+                <div>{trail.difficulty}</div>
+                <div>Length: {trail.distance} km</div>
+            </div>
+        </a>
     );
 }
 
