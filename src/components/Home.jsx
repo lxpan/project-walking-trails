25916bba0239d00ff05dc8ef381f996292bfdc0a
Luoxi Pan <l.panpax@gmail.com>
2023-05-03 14:57:59 +1000

    Add four more trails to seed file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,6 +16,7 @@ function Home() {
 
     useEffect(() => {
         // actions.trails.getTrails();
+        // actions.trails.migrateTrailsData();
         // actions.routes.migrateRoutesAction();
         // actions.routes.getRoute('creswick-circuit-walk');
         // actions.routes.getRoute('cosgrove-reservoir-loop');
