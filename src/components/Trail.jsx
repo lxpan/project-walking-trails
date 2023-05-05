64132eab71d0311d30141cc871a1a5e6b219b383
Luoxi Pan <l.panpax@gmail.com>
2023-05-05 15:31:35 +1000

    Create and style basic trail header
    - Includes placeholder breadcrumbs
    - Includes trail name, difficulty and area name

--- src/components/Trail.jsx
+++ src/components/Trail.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useAppState } from '../overmind';
+import { capitalise, unslugify } from '../utils/utils';
+import '../styles/Trail.css';
 
 function Trail() {
     const { id } = useParams();
+    const { trails } = useAppState();
+
+    const trail = trails[id];
+
+    useEffect(() => {
+        console.log(trail);
+    }, [trails]);
+
     return (
         <div className="Trail">
-            <h1>{id}</h1>
+            <div className="Trail-contents">
+                <div className="trail-breadcrumbs">{`Australia > Victoria > ${unslugify(
+                    trail.area,
+                )} > ${trail.name}`}</div>
+                <div className="trail-header">
+                    <h2 className="trail-header-trail-heading">{trail.name}</h2>
+                    <span className="trail-header-trail-difficulty">
+                        {capitalise(trail.difficulty)}
+                    </span>
+                    <span className="trail-header-trail-area">{unslugify(trail.area)}</span>
+                </div>
+                <p>{trail.description}</p>
+            </div>
         </div>
     );
 }
