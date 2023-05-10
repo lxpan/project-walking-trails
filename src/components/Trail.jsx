0f64e44eec7593190672474208224575ebd204da
Luoxi Pan <l.panpax@gmail.com>
2023-05-10 14:01:49 +1000

    Replace trail area with location name

--- src/components/Trail.jsx
+++ src/components/Trail.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppState } from '../overmind';
-import { capitalise, unslugify } from '../utils/utils';
+import { capitalise } from '../utils/utils';
 import '../styles/Trail.css';
 
 function Trail() {
@@ -10,7 +10,6 @@ function Trail() {
 
     const trail = trails[id];
     const loc = locations[trail.area];
-    const locDisplayName = `${loc.name}, ${loc.state}, ${loc.country}`;
 
     useEffect(() => {
         console.log(trail.area);
@@ -30,7 +29,7 @@ function Trail() {
                     <span className="trail-header-trail-difficulty">
                         {capitalise(trail.difficulty)}
                     </span>
-                    <span className="trail-header-trail-area">{unslugify(trail.area)}</span>
+                    <span className="trail-header-trail-area">{loc.name}</span>
                 </div>
                 <p>{trail.description}</p>
             </div>
