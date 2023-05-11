12564001057128ed2da73def4d40d37a160500ab
Luoxi Pan <l.panpax@gmail.com>
2023-05-11 15:48:16 +1000

    Fix Trail page not loading on direct load
    - Add React state variables for "trail" and "loc", with useEffect to set
    - Add rendering dependent on trail
    - Add nullish coallescing render of loc properties

--- src/components/Trail.jsx
+++ src/components/Trail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAppState } from '../overmind';
 import { capitalise } from '../utils/utils';
@@ -8,31 +8,48 @@ function Trail() {
     const { id } = useParams();
     const { trails, locations } = useAppState();
 
-    const trail = trails[id];
-    const loc = locations[trail.area];
+    /* React state variables are used to enable conditional rendering since
+    Overmind state variables don't load on time if the Trail page URL is accessed
+    directly (as opposed to the normal flow of accessing the Trail's page through Home) */
+    const [trail, setTrail] = useState();
+    const [loc, setLoc] = useState();
 
     useEffect(() => {
-        console.log(trail.area);
-        console.log(loc);
+        setTrail(trails[id]);
     }, [trails]);
+
+    /* This useEffect hook is called when either trail or locations is updated,
+    ensuring that loc (which is dependent on trail) is set after trail has been set */
+    useEffect(() => {
+        if (trail) {
+            setLoc(locations[trail.area]);
+        }
+    }, [trail, locations]);
+
+    if (!(trail && loc)) {
+        // placeholder loading message
+        return <h1 style={{ fontSize: 100 }}>Loading...</h1>;
+    }
 
     return (
         <div className="Trail">
-            <div className="Trail-contents">
-                <div className="trail-breadcrumbs">
-                    {`${capitalise(loc.country)} > ${capitalise(loc.state)} > ${loc.name} > ${
-                        trail.name
-                    }`}
+            {trail && (
+                <div className="Trail-contents">
+                    <div className="trail-breadcrumbs">
+                        {`${capitalise(loc?.country ?? 'Loading')} > ${capitalise(
+                            loc?.state ?? 'Loading',
+                        )} > ${loc?.name ?? 'Loading'} > ${trail.name}`}
+                    </div>
+                    <div className="trail-header">
+                        <h2 className="trail-header-trail-heading">{trail.name}</h2>
+                        <span className="trail-header-trail-difficulty">
+                            {capitalise(trail.difficulty)}
+                        </span>
+                        <span className="trail-header-trail-area">{loc?.name ?? 'Loading'}</span>
+                    </div>
+                    <p>{trail.description}</p>
                 </div>
-                <div className="trail-header">
-                    <h2 className="trail-header-trail-heading">{trail.name}</h2>
-                    <span className="trail-header-trail-difficulty">
-                        {capitalise(trail.difficulty)}
-                    </span>
-                    <span className="trail-header-trail-area">{loc.name}</span>
-                </div>
-                <p>{trail.description}</p>
-            </div>
+            )}
         </div>
     );
 }
