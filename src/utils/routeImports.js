f5fbd3fa6b10e7696ec57b1b226754b20ccf6b5e
Luoxi Pan <l.panpax@gmail.com>
2023-07-14 16:53:21 +1000

    Add missing gold fields geoJSON

--- src/utils/routeImports.js
+++ src/utils/routeImports.js
@@ -4,6 +4,7 @@ import cosgroveReservoir from '../geoJson/cosgrove-reservoir-loop.json';
 import creswickCircuitWalk from '../geoJson/creswick-circuit-walk.json';
 import lakeWendoureeNorth from '../geoJson/lake-wendouree-north-park.json';
 import yarroweeRiver from '../geoJson/yarrowee-river-south-trail.json';
+import goldfieldsTrack from '../geoJson/goldfields-track-oak-gully-road-and-park-lake .json';
 
 const routeImports = {
     'branding-yard-you-yangs': brandingYard,
@@ -12,5 +13,7 @@ const routeImports = {
     'creswick-circuit-walk': creswickCircuitWalk,
     'lake-wendouree-north-park': lakeWendoureeNorth,
     'yarrowee-river-south': yarroweeRiver,
+    'goldfields-track-oak-gully-road-and-park-lake': goldfieldsTrack,
 };
+
 export default routeImports;
