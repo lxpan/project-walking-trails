2bc632cc91c277a97db9dc8ff917842d0c095a38
Luoxi Pan <l.panpax@gmail.com>
2023-08-11 11:59:55 +1000

    Remove prettier import

--- src/components/Trail/Trail.jsx
+++ src/components/Trail/Trail.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { clearConfigCache } from 'prettier';
 import { useAppState } from '../../overmind';
 import { capitalise } from '../../utils/utils';
 import TrailTag from './TrailTag';
