ad818fc18ab67d9b777a710c103e1fbe23ce189a
Luoxi Pan <l.panpax@gmail.com>
2023-08-11 11:37:58 +1000

    Add rating stars to trail reviews

--- src/components/Trail/Reviews.jsx
+++ src/components/Trail/Reviews.jsx
@@ -2,23 +2,30 @@ import React from 'react';
 import ReviewStats from './ReviewStats';
 import '../../styles/Trail/Reviews.css';
 
-import profilePic from '../../assets/images/account.png';
+import profilePic from '../../assets/images/trail/account.png';
+import reviewStarsPic from '../../assets/images/trail/star.png';
 
 function Reviews({ reviews }) {
     if (!reviews) {
         return <h1 style={{ fontSize: 100 }}>Loading...</h1>;
     }
 
+    const renderRatingStars = (rating) => Array.from({ length: rating - 1 }, (_, i) => (
+        <img key={i} src={reviewStarsPic} alt=""></img>
+    ));
+
     const reviewsMap = Object.values(reviews).map((review, index) => (
         <div key={index} className="trail-review-div">
             <div className="trail-review-header">
-                <img src={profilePic} alt="" />
+                <img src={profilePic} alt="User profile picture" />
                 <div className="trail-review-user-date">
                     <span>{review.username}</span>
                     <span className="trail-review-date">{review.date}</span>
                 </div>
             </div>
-            <span>{review.rating}</span>
+            <div className="trail-review-rating-stars" title={`Rating of ${review.rating} stars`}>
+                {renderRatingStars(review.rating)}
+            </div>
             <span>{review.comment}</span>
             <div className="trail-section-divider"></div>
         </div>
