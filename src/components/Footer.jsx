ae7819c831e574376248981154be715d85e02280
Luoxi Pan <l.panpax@gmail.com>
2023-08-23 15:57:34 +1000

    Add styling and placeholder content to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import '../styles/Footer.css';
 
 function Footer() {
     return (
         <div className="Footer">
-            <h1>Footer</h1>
+            <ul className="Footer-links">
+                <h3>Explore</h3>
+                <li>
+                    <a href="">Countries</a>
+                </li>
+                <li>
+                    <a href="">Regions</a>
+                </li>
+                <li>
+                    <a href="">Cities</a>
+                </li>
+                <li>
+                    <a href="">Trails</a>
+                </li>
+            </ul>
+            <ul className="Footer-links">
+                <h3>Site</h3>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Explore</a>
+                </li>
+                <li>
+                    <a href="">Favourites</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+            </ul>
         </div>
     );
 }
