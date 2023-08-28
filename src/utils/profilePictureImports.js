c482537bd9cd8c63f84748f0d7eb8fdf65ad5cd4
Luoxi Pan <l.panpax@gmail.com>
2023-08-28 14:55:43 +1000

    Add profile pictures export

---
+++ src/utils/profilePictureImports.js
@@ -0,0 +1,29 @@
+import bear from '../assets/images/profile/icons8-bear-64.png';
+import bee from '../assets/images/profile/icons8-bee-64.png';
+import blackJaguar from '../assets/images/profile/icons8-black-jaguar-64.png';
+import butterfly from '../assets/images/profile/icons8-butterfly-64.png';
+import cat from '../assets/images/profile/icons8-cat-64.png';
+import deer from '../assets/images/profile/icons8-deer-64.png';
+import dog from '../assets/images/profile/icons8-dog-64.png';
+import elephant from '../assets/images/profile/icons8-elephant-64.png';
+import penguin from '../assets/images/profile/icons8-penguin-64.png';
+import rabbit from '../assets/images/profile/icons8-rabbit-64.png';
+import panda from '../assets/images/profile/icons8-panda-64.png';
+import salamander from '../assets/images/profile/icons8-salamander-64.png';
+
+const profilePictureImports = [
+    bear,
+    bee,
+    blackJaguar,
+    butterfly,
+    cat,
+    deer,
+    dog,
+    elephant,
+    penguin,
+    rabbit,
+    panda,
+    salamander,
+];
+
+export default profilePictureImports;
