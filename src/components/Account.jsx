62eaf01160e993e3b5f0aae9c011a9aac8b9c4aa
Luoxi Pan <l.panpax@gmail.com>
2023-08-29 15:23:49 +1000

    Display user account info

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { auth } from 'firebaseui';
+import { useAppState, useActions } from '../overmind';
+import '../styles/Account.css';
 
 function Account() {
+    const { authedUser } = useAppState();
+
+    if (!authedUser) {
+        return <h3>Please log in to access the account page.</h3>;
+    }
+
     return (
         <div className="Account-container">
             <h3>User Account</h3>
+            <ol className="Account-user-info-list">
+                <li>displayName: {authedUser.displayName}</li>
+                <li>email: {authedUser.email}</li>
+                <li>phoneNumber: {authedUser.phoneNumber ? authedUser.phoneNumber : 'null'}</li>
+                <li>photoURL: {authedUser.photoURL}</li>
+                <li>providerId: ${authedUser.providerId}</li>
+                <li>User id (uid): ${authedUser.uid}</li>
+            </ol>
         </div>
     );
 }
